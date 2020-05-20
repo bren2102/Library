a3433df5220c2054846170956a91b60be1eb7d3f
xtrmdarc <diegoreyesco@hotmail.com>
2020-05-20 17:03:53 -0500

    Add new book form and create render method

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ function Book(author, title, npages, read) {
   this.title = title;
   this.npages = npages;
   this.read = read;
+  this.html = `
+        <div class="book">
+          <h3 class="title">${this.title}</h3>
+          <span class="author">Author: ${this.author}</span>
+          <span class="num_pages"> Number of pages: ${this.npages}</span>
+          <span class="read">Read: ${this.read}</span>
+        </div>
+      `;
 }
 
 function addBookToLibrary(book,library) {
   library.push(book);
+  render(library);
 }
 
 let book1 = new Book("Drew Eric Whitman", "Cashvertising", 224, "YES");
@@ -20,7 +29,30 @@ addBookToLibrary(book2, myLibrary);
 addBookToLibrary(book3, myLibrary);
 
 function render(library){
+  let library_books = document.getElementById('library_books');
+  let books_html = ``;
   for (let i=0; i<library.length; i++){
-    library[i];
+    books_html += library[i].html;
   }
+  library_books.innerHTML = books_html;
 }
+
+render(myLibrary);
+
+document.getElementById('form_new_book').onsubmit = function(e){
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let npages = document.getElementById('num_pages').value;
+  let read = (function() {
+    if(document.getElementById('read_yes').checked == true )
+    {
+      return 'YES';
+    }
+    else {
+      return 'NO';
+    }
+  })();
+  let book = new Book(author,title, npages, read);
+
+  addBookToLibrary(book,myLibrary);
+};
